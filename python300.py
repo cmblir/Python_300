5bbdf4ebd9a2831d03150ed92c783b5214f13e16
Nicholas Yoo <sodlalwl13@gmail.com>
2022-02-24 22:50:58 +0900

    22/02/24

---
+++ python300.py
@@ -0,0 +1,24 @@
+# 1
+print('hello')
+# 2
+print("Mary's cosmetics")
+# 3
+print('신씨가 소리질렀다. "도둑이야".')
+# 4
+print('"C:\Windows"')
+# 5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+## \n은 줄바꿈, \t는 탭
+# 6
+print("오늘은", "일요일")
+# 오늘은 일요일
+# 7
+print("naver;kakao;sk;samsung")
+# 8
+print("naver/kakao/sk/samsung")
+# 9
+print("first");print("second")
+# ;는 따로 줄바꿈없이 출력이 가능하다.
+# 10
+a = 5/3
+print(a)
