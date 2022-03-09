d256dfa8f95f0785845e026c9cfe740f025658b7
Nicholas Yoo <sodlalwl13@gmail.com>
2022-03-10 00:03:42 +0900

    22.03.10

--- Python_Loop/python_151_160.py
+++ Python_Loop/python_151_160.py
@@ -1,20 +1,59 @@
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# 151
+# 리스트에는 네 개의 정수가 저장돼 있다.
+lst = [3, -20, -3, 44]
+for i in lst:
+    if i < 0:
+        print(i)
+# 152
+# for문을 사용해서 3의 배수만을 출력하라.
+lst = [3, 100, 23, 44]
+for i in lst:
+    if i % 3 ==0:
+        print(i)
+# 153
+# 리스트에서 20 보다 작은 3의 배수를 출력하라
+lst = [13, 21, 12, 14, 30, 18]
+for i in lst:
+    if i % 3 ==0 and i < 20:
+        print(i)
+# 154
+# 리스트에서 세 글자 이상의 문자를 화면에 출력하라
+lst_to = "I study python language !"
+lst = lst_to.split(" ")
+for i in lst:
+    if len(i) > 3:
+        print(i)
+# 155
+# 리스트에서 대문자만 화면에 출력하라.
+lst = ['A', 'b', 'c', 'D']
+for i in lst:
+    if i.isupper() == True:
+        print(i)
+# 156
+# 리스트에서 소문자만 화면에 출력하라.
+lst = ['A', 'b', 'c', 'D']
+for i in lst:
+    if i.isupper() == False:
+        print(i)
+# 157
+# 이름의 첫 글자를 대문자로 변경해서 출력하라.
+lst = ['dog', 'cat', 'parrot']
+for i in lst:
+    print(i[0].upper()+i[1:])
+# 158
+# 파일 이름이 저장된 리스트에서 확장자를 제거하고 파일 이름만 화면에 출력하라. (힌트: split() 메서드)
+lst = ['hello.py', 'ex01.py', 'intro.hwp']
+for i in lst:
+    print(i.split(".")[0])
+# 159
+# 파일 이름이 저장된 리스트에서 확장자가 .h인 파일 이름을 출력하라.
+lst = ['intra.h', 'intra.c', 'define.h', 'run.py']
+for i in lst:
+    if i.split(".")[1] == 'h':
+        print(i)
+# 160
+# 파일 이름이 저장된 리스트에서 확장자가 .h나 .c인 파일을 화면에 출력하라.
+lst = ['intra.h', 'intra.c', 'define.h', 'run.py']
+for i in lst:
+    if i.split(".")[1] == 'h' or i.split(".")[1] == 'c':
+        print(i)
