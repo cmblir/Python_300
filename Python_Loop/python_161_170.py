d06d98ff380ddd562c81ee2ce7c368c4a5ce0b9d
Nicholas Yoo <sodlalwl13@gmail.com>
2022-03-11 18:08:52 +0900

    22.03.11

--- Python_Loop/python_161_170.py
+++ Python_Loop/python_161_170.py
@@ -1,14 +1,44 @@
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# 161
+for i in range(1,100):
+    print(i)
+# 162
+for i in range(2002, 2051, 4):
+    print(i)
+# 163
+for i in range(1, 31):
+    if i % 3 ==0:
+        print(i)
+    else:
+        pass
+# 164
+for i in range(99, 0, -1):
+    print(i)
+# 165
+for i in range(10):
+    print(i / 10)
+# 166
+for i in range(1, 10):
+    print(f"3X{i} = {i*3}")
+# 167
+for i in range(1, 10):
+    if i % 2 == 1:
+        print(f"3X{i} = {i*3}")
+# 168
+a = 0
+for i in range(1, 11):
+    a += i
+print(f"합 : {a}")
+# 169
+b = 0
+for i in range(1, 11):
+    if i % 2 == 1:
+        b += i
+print(f"합 : {b}")
+# 170
+c = 1
+for i in range(1, 10):
+    c *= i
+print(f"곱 : {c}")
 # 
 # 
 # 
